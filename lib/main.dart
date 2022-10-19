fbc1c3136552ba4399bb99a883a1d775d3015c84
Micfong <68492819+0Micfong0@users.noreply.github.com>
2022-10-19 22:06:04 +0800

    Day 0: Add weather list

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List<Map<String, dynamic>> weathers = [
+      {'city': '深圳', 'temp': 22, 'high': 27, 'low': 18, 'weather': '多云'},
+      {'city': '三亚', 'temp': 24, 'high': 25, 'low': 21, 'weather': '小雨'},
+      {'city': '成都', 'temp': 18, 'high': 24, 'low': 14, 'weather': '多云'},
+      {'city': '北京', 'temp': 10, 'high': 15, 'low': 6, 'weather': '晴'},
+      {'city': '上海', 'temp': 12, 'high': 17, 'low': 8, 'weather': '晴'},
+      {'city': '广州', 'temp': 20, 'high': 25, 'low': 16, 'weather': '多云'},
+      {'city': '杭州', 'temp': 14, 'high': 19, 'low': 10, 'weather': '晴'},
+    ];
+
     return MaterialApp(
       title: 'My Weather App',
       home: Scaffold(
