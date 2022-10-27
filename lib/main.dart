9d2a5551d9c4c10edc178377139b856484f90cf1
Micfong <68492819+0Micfong0@users.noreply.github.com>
2022-10-27 17:03:15 +0800

    Day 2: Use state and list

--- lib/main.dart
+++ lib/main.dart
@@ -26,47 +26,87 @@ class MyApp extends StatelessWidget {
           title: const Text('My Weather App'),
         ),
         body: Center(
-          child: Column(
+          child: ListView(
             children: [
               ...[
-                for (var weather in weathers)
-                  Container(
-                      margin: const EdgeInsets.symmetric(
-                          vertical: 8, horizontal: 16),
-                      padding: const EdgeInsets.all(8),
-                      color: Colors.grey.shade200,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(weather['city'],
-                                  style: const TextStyle(
-                                      fontSize: 24,
-                                      fontWeight: FontWeight.bold)),
-                              Text('${weather['temp']}°',
-                                  style: const TextStyle(
-                                      fontSize: 24,
-                                      fontWeight: FontWeight.bold)),
-                            ],
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(weather['weather']),
-                              Text(
-                                  '最高 ${weather['high']}° 最低 ${weather['low']}°'),
-                            ],
-                          ),
-                        ],
-                      )),
+                for (var weather in weathers) WeatherRow(weather: weather),
               ],
             ],
           ),
         ),
       ),
     );
+  }
+}
+
+class WeatherRow extends StatefulWidget {
+  const WeatherRow({
+    Key? key,
+    required this.weather,
+  }) : super(key: key);
+
+  final Map<String, dynamic> weather;
+
+  @override
+  State<WeatherRow> createState() => _WeatherRowState();
+}
+
+class _WeatherRowState extends State<WeatherRow> {
+  bool favorite = false;
+  // DECLARE Favorite: BOOLEAN
+  // Favorite <- FALSE
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+        padding: const EdgeInsets.all(8),
+        color: Colors.grey.shade200,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Row(
+              children: [
+                Text(widget.weather['city'],
+                    style: const TextStyle(
+                        fontSize: 24, fontWeight: FontWeight.bold)),
+                MaterialButton(
+                    child: Row(children: [
+                      favorite
+                          ? const Icon(Icons.star, color: Colors.amber)
+                          : const Icon(Icons.star_outline),
+                      // IF Favorite = TRUE THEN
+                      //   "已收藏"
+                      // ELSE
+                      //   "收藏"
+                      // ENDIF
+                      Container(
+                        width: 8,
+                      ),
+                      Text(favorite ? "已收藏" : "收藏",
+                          style: const TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.bold))
+                    ]),
+                    onPressed: () {
+                      setState(() {
+                        favorite = !favorite;
+                      });
+                    }),
+                const Spacer(),
+                Text('${widget.weather['temp']}°',
+                    style: const TextStyle(
+                        fontSize: 24, fontWeight: FontWeight.bold)),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(widget.weather['weather']),
+                Text(
+                    '最高 ${widget.weather['high']}° 最低 ${widget.weather['low']}°'),
+              ],
+            ),
+          ],
+        ));
   }
 }
